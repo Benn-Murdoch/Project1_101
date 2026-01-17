6074c5a74c58f7afe7684293246b82b1ae610f66
Bennjamen Murdoch <bsmurdoc@calpoly.edu>
2026-01-17 01:10:45 +0000

    first try edited

--- p1.py
+++ p1.py
@@ -2,10 +2,10 @@ def convert_case(character, target_case):
     character_num = ord(character) 
     
     if 65 <= character_num <= 90 or 97 <= character_num <= 122:
-        if target_case == ("upper" or "Upper" or "UPPER") :
+        if target_case == ("upper" or "Upper" or "UPPER") and character_num >=97 :
             new_num = character_num + 32
             return chr(new_num)
-        elif target_case == ("lower" or "Lower" or "LOWER") : 
+        elif target_case == ("lower" or "Lower" or "LOWER") and character_num <=90: 
             new_num = character_num - 32
             return chr(new_num)
         else:
