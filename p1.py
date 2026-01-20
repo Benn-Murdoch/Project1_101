3c8e70d06e95422b0a231b83bd99a6ba41b5cfee
Bennjamen Murdoch <bsmurdoc@calpoly.edu>
2026-01-20 04:34:09 +0000

    figured it out

--- p1.py
+++ p1.py
@@ -1,17 +1,19 @@
 def convert_case(character, target_case):
-    character_num = ord(character) 
-    
-    if 65 <= character_num <= 90 or 97 <= character_num <= 122:
-        if target_case == ("upper" or "Upper" or "UPPER") and character_num >=97 :
-            new_num = character_num + 32
-            return chr(new_num)
-        elif target_case == ("lower" or "Lower" or "LOWER") and character_num <=90: 
-            new_num = character_num - 32
-            return chr(new_num)
-        else:
-            return chr(character_num)
+       
+    if len(character) <= 1:   
+        character_num = ord(character) 
+        if 65 <= character_num <= 90 or 97 <= character_num <= 122:
+            if target_case == ("upper" or "Upper" or "UPPER") and character_num >=97 :
+                new_num = character_num - 32
+                return chr(new_num)
+            elif target_case == ("lower" or "Lower" or "LOWER") and character_num <=90: 
+                new_num = character_num + 32
+                return chr(new_num)
+            else:
+                return chr(character_num)
 
+    
     else: 
         return character
 
-print(convert_case("a", "upper"))
+print(convert_case("A", "hi"))
